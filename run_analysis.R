06042f1b1b11fbe9ebaeb1bbfdd5e836363435c6
vijji139 <96228237+vijji139@users.noreply.github.com>
2023-11-20 16:35:15 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,27 @@
+# Loading required packages
+
+filename <- "Coursera.zip"
+
+# Checking if archieve already exists.
+if (!file.exists(filename)){
+  fileURL <- "Downloads/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+  download.file(fileURL, filename, method="curl")
+}  
+
+# Checking if folder exists
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+# Data frames
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
